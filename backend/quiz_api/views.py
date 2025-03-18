3b385fbefb3192985b6b48fff7b6664b671e8c46
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-03-18 19:12:16 +0000

    view api created

---
+++ backend/quiz_api/views.py
@@ -0,0 +1,20 @@
+from backend.instances.questions import quiz_questions
+from django.http import JsonResponse
+def getdummyquestions(request):
+    """Return a list of dummy quiz questions."""
+    return JsonResponse([{
+        "question_text": question.question_text,
+        "correct_answers": question.correct_answers,
+        "wrong_answers": question.wrong_answers,
+        "explanation_correct": question.explanation_correct,
+        "explanation_wrong": question.explanation_wrong
+    } for question in quiz_questions])
+# Compare this snippet from backend/quiz_api/urls.py:
+# from django.urls import path
+# from . import views
+#
+
+# urlpatterns = [
+#     path('questions/', views.getdummyquestions),
+# ]
+# Compare this snippet from backend/quiz_api/views.py:        
