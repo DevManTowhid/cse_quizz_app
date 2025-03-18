462c6f1d4ec6b85cc37b6d13cf08b0978924e47c
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-03-18 19:37:51 +0000

    In order to allow non-dict objects to be serialized set the safe parameter to False.

--- backend/quiz_api/views.py
+++ backend/quiz_api/views.py
@@ -1,14 +1,15 @@
-from backend.instances.questions import quiz_questions
+from instances.questions import quiz_questions
 from django.http import JsonResponse
 def getdummyquestions(request):
     """Return a list of dummy quiz questions."""
     return JsonResponse([{
+        "id": question.id,
         "question_text": question.question_text,
         "correct_answers": question.correct_answers,
         "wrong_answers": question.wrong_answers,
         "explanation_correct": question.explanation_correct,
         "explanation_wrong": question.explanation_wrong
-    } for question in quiz_questions])
+    } for question in quiz_questions], safe=False)
 # Compare this snippet from backend/quiz_api/urls.py:
 # from django.urls import path
 # from . import views
