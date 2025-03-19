a0e09fa3608b1d2bf6f28e6e38021efb381500b5
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-03-19 21:54:43 +0000

    urls added push dummy data

--- backend/quiz_api/urls.py
+++ backend/quiz_api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import getdummyquestions  # Import your view
+from .views import getdummyquestions, push_dummy_quiz_data  # Import your views
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # Ensure Django admin is accessible
     path("dummy-quiz/", getdummyquestions, name="dummy-quiz"),
+    path('push-quiz-data/', push_dummy_quiz_data, name='push-quiz-data')
 ]
